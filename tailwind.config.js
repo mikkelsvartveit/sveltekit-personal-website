c4ba97c41af212490f07c4b6dfd5728f43839120
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-04-11 15:53:08 +0200

    Make monospace font more consistent

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
       sans: "'Nunito', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
       serif:
         "'Source Serif Pro', ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif",
-      mono: "'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace'",
+      mono: "Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
     },
     extend: {},
   },
