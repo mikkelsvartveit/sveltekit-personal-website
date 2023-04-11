3337427d9118b933fd9f16ac2b1d76103f13fcc9
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-04-11 11:21:36 +0200

    Improve photos layout on mobile

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -5,21 +5,23 @@
   const { photos } = data;
 </script>
 
-<div class="mx-auto px-4 py-12">
+<div class="max-w-4xl mx-auto px-3 sm:px-6 pt-12">
   <h1
     class="font-serif font-light text-3xl sm:text-4xl text-gray-600 tracking-wide mb-8 text-center"
   >
     Photography
   </h1>
 
-  <Paragraph class="max-w-4xl mx-auto px-3 sm:px-6">
+  <Paragraph>
     I enjoy taking photos and I always bring my camera with me when I travel. I
     used to shoot with a Nikon DSLR, but recently switched to a Fujifilm X-T20,
     which I find a lot more enjoyable to bring around. These are some of my
     favorite shots from over the years.
   </Paragraph>
+</div>
 
-  <section class="columns-2 md:columns-3 lg:columns-4 gap-4 mt-12">
+<div class="mx-auto px-4">
+  <section class="columns-2 md:columns-3 lg:columns-4 gap-4 mt-12 mb-2">
     {#each photos as photo}
       <a
         href="/photos-optimized/{photo.src}/full.jpg"
